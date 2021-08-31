92f56754d739ed287429e65cf1c0e65d2a51eba4
Arcadia Roleplay <acehighrp@gmail.com>
2021-08-31 02:00:34 +0100

    Initial Upload

---
+++ client/client.lua
@@ -0,0 +1,65 @@
+local shot = false
+local check = false
+local check2 = false
+local count = 0
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		if IsPlayerFreeAiming(PlayerId()) then
+		    if GetFollowPedCamViewMode() == 4 and check == false then
+			    check = false
+			else
+			    SetFollowVehicleCamViewMode(4)
+			    check = true
+			end
+		else
+		    if check == true then
+		        SetFollowVehicleCamViewMode(1)
+				check = false
+			end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		if IsPedShooting(GetPlayerPed(-1)) and shot == false and GetFollowPedCamViewMode() ~= 4 then
+			check2 = true
+			shot = true
+			SetFollowVehicleCamViewMode(4)
+		end
+		if IsPedShooting(GetPlayerPed(-1)) and shot == true and GetFollowPedCamViewMode() == 4 then
+			count = 0
+		end
+		if not IsPedShooting(GetPlayerPed(-1)) and shot == true then
+		    count = count + 1
+		end
+        if not IsPedShooting(GetPlayerPed(-1)) and shot == true then
+			if not IsPedShooting(GetPlayerPed(-1)) and shot == true and count > 20 then
+		        if check2 == true then
+				    check2 = false
+					shot = false
+					SetFollowVehicleCamViewMode(1)
+				end
+			end
+		end
+	end
+end)
+
+-- roll Prevention
+Citizen.CreateThread(function()
+	while true do
+		if (not IsPedInAnyVehicle(PlayerPedId(),false)) then
+			Citizen.Wait(4)
+			if IsPlayerFreeAiming(PlayerPedId()) then
+				DisableControlAction(0, 22, 1)
+			else
+				Citizen.Wait(100)
+			end
+		else
+			Citizen.Wait(500)
+		end
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Vehicle Force Camera'
+version '1.0.0'
+author 'Nathan#8860'
+
+client_script 'client/*.lua'
